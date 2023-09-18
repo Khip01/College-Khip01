7881c2236bf42509af789c203187bc3067bde153
Khip01 <aakhifathallah24@gmail.com>
2023-09-18 12:51:57 +0700

    Update hargaBayar changes

--- Jobsheet-4/HargaBayar02.java
+++ Jobsheet-4/HargaBayar02.java
@@ -6,18 +6,27 @@ public class HargaBayar02 {
         
         Scanner input = new Scanner(System.in);
 
-        int harga, jumlah;
-        double dis = 0.1, total, bayar, jmlDis;
+        int harga, jumlah, jumHalBuku;
+        double dis, total, bayar, jmlDis;
+        String merk;
 
+        System.out.println("Masukkan merk buku yang dibeli ");
+        merk = input.nextLine();
+        System.out.println("Masukkan jumlah halaman buku yang dibeli ");
+        jumHalBuku = input.nextInt();
         System.out.println("Masukkan harga barang yang dibeli ");
         harga = input.nextInt();
         System.out.println("Masukkan Jumlah jumlah barang yang dibeli ");
         jumlah = input.nextInt();
+        System.out.println("Masukkan diskon dari barang yang dibeli ");
+        dis = input.nextDouble();
 
         total = harga * jumlah;
         jmlDis = total * dis;
         bayar = total - jmlDis;
 
+        System.out.println("Merk dari buku yang dibeli adalah " +merk);
+        System.out.println("Jumlah halaman buku ada " +jumHalBuku);
         System.out.println("Diskon ang anda dapatkan adalah " +jmlDis);
         System.out.println("Jumlah yang harus dibayar adalah " +bayar);
     }
