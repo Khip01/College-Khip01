5218d8e54cff53394c002114d119bb7312de21cc
Khip01 <aakhifathallah24@gmail.com>
2023-09-18 11:03:08 +0700

    Membuat File baru

---
+++ Jobsheet-4/HargaBayar02.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class HargaBayar02 {
+    
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+
+        
+
+    }
+
+}
