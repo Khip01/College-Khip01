ab5e2d37b76c9877c8b42cb82bf1a7919eeb05e0
Khip01 <aakhifathallah24@gmail.com>
2023-09-18 10:19:46 +0700

    Menambahkan Scanner dan Deklarasi Variabel

--- Jobsheet-4/Gaji02.java
+++ Jobsheet-4/Gaji02.java
@@ -1,3 +1,10 @@
+import java.util.Scanner;
+
 public class Gaji02 {
     
+    Scanner input = new Scanner(System.in);
+
+    int jmlMasuk, jmlTdkMasuk, TotGaji;
+    int gaji = 40000, potGaji = 25000;
+
 }
