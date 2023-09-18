81154a13d6b58153a242ca02df5f4f083bd320ac
Khip01 <aakhifathallah24@gmail.com>
2023-09-18 09:18:10 +0700

    Variable Declaration

--- Jobsheet-4/Lingkaran02.java
+++ Jobsheet-4/Lingkaran02.java
@@ -6,8 +6,13 @@ import java.util.Scanner;
 public class Lingkaran02 {
 
     public static void main(String[] args) {
-        
+        // Scanner Declaration
         Scanner input = new Scanner(System.in);
+
+        // Variable Declaration
+        int r;
+        double keliling, luas;
+
         
 
     }
