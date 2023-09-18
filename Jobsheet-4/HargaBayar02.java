f2b5a4840154e26b0d1015079759c256b7dda726
Khip01 <aakhifathallah24@gmail.com>
2023-09-18 11:07:44 +0700

    Input User

--- Jobsheet-4/HargaBayar02.java
+++ Jobsheet-4/HargaBayar02.java
@@ -8,7 +8,11 @@ public class HargaBayar02 {
 
         int harga, jumlah;
         double dis = 0.1, total, bayar, jmlDis;
-        
+
+        System.out.println("Masukkan harga barang yang dibeli ");
+        harga = input.nextInt();
+        System.out.println("Masukkan Jumlah jumlah barang yang dibeli ");
+        jumlah = input.nextInt();
 
     }
 
