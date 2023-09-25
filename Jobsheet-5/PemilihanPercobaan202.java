2806cfef646d78e15640939c687dc931107167ec
Khip01 <aakhifathallah24@gmail.com>
2023-09-25 17:03:00 +0700

    Menambahkan perintah untuk memasukkan inputan

--- Jobsheet-5/PemilihanPercobaan202.java
+++ Jobsheet-5/PemilihanPercobaan202.java
@@ -6,7 +6,14 @@ public class PemilihanPercobaan202 {
         
         Scanner input02 = new Scanner(System.in);
 
-        
+        System.out.println("Nilai uas   : ");
+        float uas = input02.nextFloat();
+        System.out.println("Nilai uts   : ");
+        float uts = input02.nextFloat();
+        System.out.println("Nilai kuis  : ");
+        float kuis = input02.nextFloat();
+        System.out.println("Nilai tugas : ");
+        float tugas = input02.nextFloat();
 
     }
 
