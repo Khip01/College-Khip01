96f0128e052840775530de6e43361ab7d90aea24
Khip01 <aakhifathallah24@gmail.com>
2023-09-25 16:59:21 +0700

    Menambahkan Object Scanner

---
+++ Jobsheet-5/PemilihanPercobaan202.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class PemilihanPercobaan202 {
+    
+    public static void main(String[] args) {
+        
+        Scanner input02 = new Scanner(System.in);
+
+        
+
+    }
+
+}
