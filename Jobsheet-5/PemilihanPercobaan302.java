da2aba8339507f4711fbb94c508d7cbbab61319c
Khip01 <aakhifathallah24@gmail.com>
2023-09-25 21:20:52 +0700

    Add Input Field

--- Jobsheet-5/PemilihanPercobaan302.java
+++ Jobsheet-5/PemilihanPercobaan302.java
@@ -9,7 +9,12 @@ public class PemilihanPercobaan302 {
         double angka1, angka2, hasil;
         char operator;
 
-        
+        System.out.print("Masukkan angka pertama: ");
+        angka1 = input02.nextDouble();
+        System.out.print("masukkan angka kedua: ");
+        angka2 = input02.nextDouble();
+        System.out.print("Masukkan operator (+ - * /): ");
+        operator = input02.next().charAt(0);
 
     }
 
