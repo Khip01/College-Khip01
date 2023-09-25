7e91d6228ef62218e325ac300be4c18a56c702dc
Khip01 <aakhifathallah24@gmail.com>
2023-09-25 18:07:13 +0700

    Menambahkan Nilai Huruf

--- Jobsheet-5/PemilihanPercobaan202.java
+++ Jobsheet-5/PemilihanPercobaan202.java
@@ -20,6 +20,27 @@ public class PemilihanPercobaan202 {
         String message = total < 65 ? "Remidi" : "Tidak remidi";
 
         System.out.println("Nilai akhir = "+total+" sehingga "+message);
+        
+        // Menambahkan nilai huruf
+        String nilaiHuruf;
+
+        if (total > 80 && total <= 100) {
+            nilaiHuruf = "A";
+        } else if( total > 73 && total <= 80){
+            nilaiHuruf = "B+";
+        } else if( total > 65 && total <= 73){
+            nilaiHuruf = "B";
+        } else if( total > 60 && total <= 65){
+            nilaiHuruf = "C+";
+        } else if( total > 50 && total <= 60){
+            nilaiHuruf = "C";
+        } else if( total > 39 && total <= 50){
+            nilaiHuruf = "D";
+        } else {
+            nilaiHuruf = "E";
+        }
+
+        System.out.println("Nilai huruf yang didapatkan adalah : "+nilaiHuruf);
     }
 
 }
