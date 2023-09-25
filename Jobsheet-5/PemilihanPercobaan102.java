a8213d283e95bc925b1912395d2738c4240b7a0d
Khip01 <aakhifathallah24@gmail.com>
2023-09-25 13:01:13 +0700

    Menambahkan code untuk menerima input

--- Jobsheet-5/PemilihanPercobaan102.java
+++ Jobsheet-5/PemilihanPercobaan102.java
@@ -5,6 +5,10 @@ import java.util.Scanner;
 public class PemilihanPercobaan102 {
 
     Scanner input02 = new Scanner(System.in);
+
+    System.out.print("Masukkan angka: ");
+    int angka = input02.nextInt();
+
     
     
 }
