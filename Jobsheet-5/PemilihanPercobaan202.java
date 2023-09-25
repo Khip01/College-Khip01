4b283bb04dd53ba93f4defd13fe18dcae21ee999
Khip01 <aakhifathallah24@gmail.com>
2023-09-25 17:09:40 +0700

    Menambahkan Kondisi Remidi/Tidak

--- Jobsheet-5/PemilihanPercobaan202.java
+++ Jobsheet-5/PemilihanPercobaan202.java
@@ -15,7 +15,11 @@ public class PemilihanPercobaan202 {
         System.out.println("Nilai tugas : ");
         float tugas = input02.nextFloat();
 
-        float total = (uas * 0.4F) + (uts * 0.3F) + (kuis * 0.1F) + (tugas * 0.2);
+        float total = (uas * 0.4F) + (uts * 0.3F) + (kuis * 0.1F) + (tugas * 0.2F);
+
+        String message = total < 65 ? "Remidi" : "Tidak remidi";
+
+        
     }
 
 }
