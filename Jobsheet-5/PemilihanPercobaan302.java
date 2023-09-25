1738d6501a432bcb1409b0adb25c1ca816c62cae
Khip01 <aakhifathallah24@gmail.com>
2023-09-25 21:24:22 +0700

    Add Condition using switch case

--- Jobsheet-5/PemilihanPercobaan302.java
+++ Jobsheet-5/PemilihanPercobaan302.java
@@ -16,6 +16,25 @@ public class PemilihanPercobaan302 {
         System.out.print("Masukkan operator (+ - * /): ");
         operator = input02.next().charAt(0);
 
+        switch (operator) {
+            case '+':
+                hasil = angka1 + angka2;
+                System.out.println(angka1+" + "+angka2+" = "+hasil);
+                break;
+            case '-':
+                hasil = angka1 - angka2;
+                System.out.println(angka1+" - "+angka2+" = "+hasil);
+                break;
+            case '*':
+                hasil = angka1 * angka2;
+                System.out.println(angka1+" * "+angka2+" = "+hasil);
+                break;
+            case '/':
+                hasil = angka1 / angka2;
+                System.out.println(angka1+" / "+angka2+" = "+hasil);
+                break;
+        }
+
     }
 
 }
