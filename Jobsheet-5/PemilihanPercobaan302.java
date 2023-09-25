84cb4cf46818581bdcb0ed0532810f32c0b9f96f
Khip01 <aakhifathallah24@gmail.com>
2023-09-25 21:51:37 +0700

    Experiment Remove Break at the first case

--- Jobsheet-5/PemilihanPercobaan302.java
+++ Jobsheet-5/PemilihanPercobaan302.java
@@ -20,7 +20,6 @@ public class PemilihanPercobaan302 {
             case '+':
                 hasil = angka1 + angka2;
                 System.out.println(angka1+" + "+angka2+" = "+hasil);
-                break;
             case '-':
                 hasil = angka1 - angka2;
                 System.out.println(angka1+" - "+angka2+" = "+hasil);
