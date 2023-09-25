1d42029057f87a4e6df3903c9bde388c542ed419
Khip01 <aakhifathallah24@gmail.com>
2023-09-25 17:21:59 +0700

    Fix

--- Jobsheet-5/PemilihanPercobaan202.java
+++ Jobsheet-5/PemilihanPercobaan202.java
@@ -6,13 +6,13 @@ public class PemilihanPercobaan202 {
         
         Scanner input02 = new Scanner(System.in);
 
-        System.out.println("Nilai uas   : ");
+        System.out.print("Nilai uas   : ");
         float uas = input02.nextFloat();
-        System.out.println("Nilai uts   : ");
+        System.out.print("Nilai uts   : ");
         float uts = input02.nextFloat();
-        System.out.println("Nilai kuis  : ");
+        System.out.print("Nilai kuis  : ");
         float kuis = input02.nextFloat();
-        System.out.println("Nilai tugas : ");
+        System.out.print("Nilai tugas : ");
         float tugas = input02.nextFloat();
 
         float total = (uas * 0.4F) + (uts * 0.3F) + (kuis * 0.1F) + (tugas * 0.2F);
