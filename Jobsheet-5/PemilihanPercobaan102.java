2beb40210fe76b43eda53ba9e3fdac8c1537a6ba
Khip01 <aakhifathallah24@gmail.com>
2023-09-25 13:05:04 +0700

    Menambahkan kondisi

--- Jobsheet-5/PemilihanPercobaan102.java
+++ Jobsheet-5/PemilihanPercobaan102.java
@@ -4,11 +4,18 @@ import java.util.Scanner;
  */
 public class PemilihanPercobaan102 {
 
-    Scanner input02 = new Scanner(System.in);
-
-    System.out.print("Masukkan angka: ");
-    int angka = input02.nextInt();
-
+    public static void main(String[] args) {
+        Scanner input02 = new Scanner(System.in);
     
-    
+        System.out.print("Masukkan angka: ");
+        int angka = input02.nextInt();
+        
+        if (angka % 2 == 0) {
+            System.out.println("Angka "+angka+" bilangan genap");
+        } else {
+            System.out.println("Angka "+angka+" bilangan ganjil");
+        }
+
+    }
+
 }
