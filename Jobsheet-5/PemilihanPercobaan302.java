4974fd07745f64275affbe1c6e4cb06dc42346b6
Khip01 <aakhifathallah24@gmail.com>
2023-09-25 21:16:47 +0700

    Create new File and Declare Scanner

---
+++ Jobsheet-5/PemilihanPercobaan302.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class PemilihanPercobaan302 {
+    
+    public static void main(String[] args) {
+        
+        Scanner input02 = new Scanner(System.in);
+
+
+        
+
+    }
+
+}
