817683f91cb2543333048e17f36940164bed34c8
Khip01 <aakhifathallah24@gmail.com>
2023-10-02 19:48:26 +0700

    Deklarasi Scanner

--- Jobsheet-6/Pemilihan2Percobaan102.java
+++ Jobsheet-6/Pemilihan2Percobaan102.java
@@ -1,12 +1,13 @@
+import java.util.Scanner;
+
 /**
  * Pemilihan2Percobaan102
  */
 public class Pemilihan2Percobaan102 {
 
     public static void main(String[] args) {
+        Scanner input02 = new Scanner(System.in);
         
-        
-
     }
 
 }
