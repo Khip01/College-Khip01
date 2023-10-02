71b695c5cc594bd110ec1ebdcdc16efbd087d09e
Khip01 <aakhifathallah24@gmail.com>
2023-10-02 20:40:00 +0700

    Menambahkan 2000 (pengecualian)

--- Jobsheet-6/Pemilihan2Percobaan102.java
+++ Jobsheet-6/Pemilihan2Percobaan102.java
@@ -14,6 +14,8 @@ public class Pemilihan2Percobaan102 {
         if ((tahun % 4) == 0 ) {
             if ((tahun % 100) != 0) {
                 System.out.println("Tahun Kabisat");
+            } else if (tahun == 2000){
+                System.out.println("Tahun Kabisat");
             } else {
                 System.out.println("Bukan Tahun Kabisat");
             }
