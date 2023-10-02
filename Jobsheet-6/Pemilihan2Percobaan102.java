487047552ece8bfb529f2a2bc227f69fa73e1765
Khip01 <aakhifathallah24@gmail.com>
2023-10-02 19:53:07 +0700

    Menambahkan kondisi

--- Jobsheet-6/Pemilihan2Percobaan102.java
+++ Jobsheet-6/Pemilihan2Percobaan102.java
@@ -7,7 +7,17 @@ public class Pemilihan2Percobaan102 {
 
     public static void main(String[] args) {
         Scanner input02 = new Scanner(System.in);
-        
+
+        System.out.print("masukkan Tahun: ");
+        int tahun = input02.nextInt();
+
+        if ((tahun % 4) == 0 ) {
+            if ((tahun % 100) != 0) {
+                System.out.println("Tahun Kabisat");
+            }
+        } else {
+            System.out.println("Bukan Tahun Kabisat");
+        }
     }
 
 }
