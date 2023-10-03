d7d51819e5ac54fbee626a374aad09618ee864f1
Khip01 <aakhifathallah24@gmail.com>
2023-10-03 07:22:17 +0700

    Perintah untuk memasukkan input

--- Jobsheet-6/Pemilihan2Percobaan202.java
+++ Jobsheet-6/Pemilihan2Percobaan202.java
@@ -4,5 +4,14 @@ public class Pemilihan2Percobaan202 {
     public static void main(String[] args) {
         Scanner input02 = new Scanner(System.in);
         
+        // Perintah memasukkan input dari user
+        System.out.print("Masukkan sudut pertama: ");
+        int sudut1 = input02.nextInt();
+        System.out.print("Masukkan sudut kedua: ");
+        int sudut2 = input02.nextInt();
+        System.out.print("Masukkan sudut ketiga: ");
+        int sudut3 = input02.nextInt();
+        
+        
     }
 }
