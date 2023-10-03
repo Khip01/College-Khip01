3a9848b08665783f15fcca20926f8fe263aee166
Khip01 <aakhifathallah24@gmail.com>
2023-10-03 10:36:38 +0700

    Deklarasi var kategori, penghasilan, gajiBersih

--- Jobsheet-6/Pemilihan2Percobaan302.java
+++ Jobsheet-6/Pemilihan2Percobaan302.java
@@ -3,10 +3,14 @@ import java.util.Scanner;
 public class Pemilihan2Percobaan302 {
     
     public static void main(String[] args) {
-        
+
         Scanner input02 = new Scanner(System.in);
 
+        String kategori;
+        int penghasilan, gajiBersih;
+        double pajak = 0;
 
+        
     }
 
 }
