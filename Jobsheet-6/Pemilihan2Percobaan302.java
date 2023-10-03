1f7490b7fd015a850176e7b13ae4e297dbd594f4
Khip01 <aakhifathallah24@gmail.com>
2023-10-03 11:02:51 +0700

    Menambahkan Kondisi

--- Jobsheet-6/Pemilihan2Percobaan302.java
+++ Jobsheet-6/Pemilihan2Percobaan302.java
@@ -14,6 +14,26 @@ public class Pemilihan2Percobaan302 {
         kategori = input02.nextLine();
         System.out.print("Masukkan Besarnya Penghasilan : ");
         penghasilan = input02.nextInt();
+
+        if (kategori.equalsIgnoreCase("pekerja")) {
+            if (penghasilan <= 2000000)
+                pajak = 0.1;
+            else if (penghasilan <= 3000000)
+                pajak = 0.15;
+            else
+                pajak = 0.2;
+            gajiBersih = (int) (penghasilan - (pajak * penghasilan));
+            System.out.print("Penghasil Bersih : " + gajiBersih);
+        } else if (kategori.equalsIgnoreCase("pebisnis")){
+            if (penghasilan <= 2500000)
+                pajak = 0.15;
+            else if (penghasilan <= 3500000)
+                pajak = 0.2;
+            else
+                pajak = 0.25;
+            gajiBersih = (int) (penghasilan - (pajak * penghasilan));
+            System.out.print("Penghasil Bersih : " + gajiBersih);
+        }
     }
 
 }
