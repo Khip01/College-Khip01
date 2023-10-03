348b927e5f5545020ce74fda803e8a7b2b273438
Khip01 <aakhifathallah24@gmail.com>
2023-10-03 09:08:57 +0700

    Add: Menambahkan

--- Jobsheet-6/Pemilihan2Percobaan202.java
+++ Jobsheet-6/Pemilihan2Percobaan202.java
@@ -17,6 +17,12 @@ public class Pemilihan2Percobaan202 {
         if (totalSudut == 180) {
             if ((sudut1 == 90) || (sudut2 == 90) || (sudut3 == 90))
                 System.out.println("Segitiga tersebut adalah segitiga sikut-siku");
+            else if ((sudut1 == 60) && (sudut2 == 60) && (sudut3 == 60))
+                System.out.println("Segitiga tersebut adalah segitiga sama sisi");
+            else if ((sudut1 == sudut2) || (sudut2 == sudut3) || (sudut3 == sudut1))
+                System.out.println("Segitiga tersebut adalah segitiga sama kaki");
+            else if ((sudut1 != sudut2) && (sudut2 != sudut3) && (sudut3 != sudut1))
+                System.out.println("Segitiga tersebut adalah segitiga sembarang");
             else
                 System.out.println("Segitiga tersebut adalah bukan segitiga siku-siku");
         } else 
