11ff01cde0676a693416210d2ef80fb250275343
Khip01 <aakhifathallah24@gmail.com>
2023-10-11 20:31:26 +0700

    Menambahkan input dari keyboard

--- Jobsheet-7/ForKelipatan02.java
+++ Jobsheet-7/ForKelipatan02.java
@@ -8,6 +8,9 @@ public class ForKelipatan02 {
         Scanner scan = new Scanner(System.in);
 
         int kelipatan, jumlah = 0, counter = 0;
+
+        System.out.print("Masukkan kelipatan (1-9): ");
+        kelipatan = scan.nextInt();
     }
 
 }
