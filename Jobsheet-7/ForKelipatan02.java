0244d4be59b5a3a4f3a54d719a5dd4e57cc1286f
Khip01 <aakhifathallah24@gmail.com>
2023-10-11 20:53:45 +0700

    Menambahkan looping for

--- Jobsheet-7/ForKelipatan02.java
+++ Jobsheet-7/ForKelipatan02.java
@@ -7,10 +7,17 @@ public class ForKelipatan02 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        int kelipatan, jumlah = 0, counter = 0;
+        int kelipatan, total = 0, counter = 0;
 
         System.out.print("Masukkan kelipatan (1-9): ");
         kelipatan = scan.nextInt();
+
+        for (int i = 1; i <= 50; i++) {
+            if (i % kelipatan == 0) {
+                total += i;
+                counter++;
+            }
+        }
     }
 
 }
