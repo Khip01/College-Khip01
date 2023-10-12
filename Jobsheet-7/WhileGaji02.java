89b86e35c21046206c076df8fe6f3cc4513239ed
Khip01 <aakhifathallah24@gmail.com>
2023-10-12 20:06:44 +0700

    feat: deklarasi variabel

--- Jobsheet-7/WhileGaji02.java
+++ Jobsheet-7/WhileGaji02.java
@@ -6,5 +6,12 @@ public class WhileGaji02 {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
+        int jumlahKaryawan, jumlahJamLembur;
+        double gajiLembur = 0, totalGajiLembur = 0;
+
+        String jabatan;
+
+        
     }
 }
