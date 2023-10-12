7cf5047afe17035d589b76b6fe9cf531c567c9e7
Khip01 <aakhifathallah24@gmail.com>
2023-10-12 20:18:27 +0700

    feat: Menambahkan While Loop

--- Jobsheet-7/WhileGaji02.java
+++ Jobsheet-7/WhileGaji02.java
@@ -14,5 +14,25 @@ public class WhileGaji02 {
 
         System.out.print("Masukkan jumlah karyawan: ");
         jumlahKaryawan = scan.nextInt();
+
+        int i = 0;
+        while (i < jumlahKaryawan) {
+            System.out.println("Pilihan jabatan - Direktur, Manajer, Karyawan");
+            System.out.print("Masukkan jabatan karyawan ke-"+(i+1)+": ");
+            jabatan = scan.next();
+            System.out.print("Masukkan jumlah jam lembur: ");
+            jumlahJamLembur = scan.nextInt();
+            i++;
+
+            if (jabatan.equalsIgnoreCase("direktur")) {
+                continue;
+            } else if (jabatan.equalsIgnoreCase("manajer")){
+                gajiLembur = jumlahJamLembur * 100000;
+            } else if (jabatan.equalsIgnoreCase("karyawan")){
+                gajiLembur = jumlahJamLembur * 75000;
+            }
+
+            totalGajiLembur += gajiLembur;
+        }
     }
 }
