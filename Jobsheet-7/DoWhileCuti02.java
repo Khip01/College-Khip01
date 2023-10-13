3117cc6e6a2e801cd824ec3e1bb00464b3ce2151
Khip01 <aakhifathallah24@gmail.com>
2023-10-13 07:19:51 +0700

    feat:Menambahkan Do While

--- Jobsheet-7/DoWhileCuti02.java
+++ Jobsheet-7/DoWhileCuti02.java
@@ -8,6 +8,25 @@ public class DoWhileCuti02 {
         int jatahCuti, jumlahHari;
         String konfirmasi; 
 
+        System.out.print("Jatah cuti : ");
+        jatahCuti = sc.nextInt();
         
+        do {
+            System.out.print("Apakah Anda ingin mengambil cuti (y/t)? ");
+            konfirmasi = sc.next();
+
+            if (konfirmasi.equalsIgnoreCase("y")) {
+                System.out.print("Jumlah hari: ");
+                jumlahHari = sc.nextInt();
+
+                if (jumlahHari <= jatahCuti) {
+                    jatahCuti -= jumlahHari;
+                    System.out.println("Sisa jatah cuti: "+jatahCuti);
+                } else {
+                    System.out.println("Sisa jatah cuti Anda tidak mencukupi ");
+                    break;
+                }
+            }
+        } while (jatahCuti > 0 );
     }
 }
