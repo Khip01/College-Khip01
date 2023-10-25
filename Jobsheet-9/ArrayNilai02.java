7a67cbaed244c23bfc2e1d0ef4b8f8610c685653
Khip01 <aakhifathallah24@gmail.com>
2023-10-25 08:25:02 +0700

    feat: add scanner declare and input field

--- Jobsheet-9/ArrayNilai02.java
+++ Jobsheet-9/ArrayNilai02.java
@@ -4,7 +4,14 @@ public class ArrayNilai02 {
     
     public static void main(String[] args) {
         
+        Scanner sc = new Scanner(System.in);
+
         int[] nilaiAkhir = new int[10];
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Masukkan nilai akhir ke-"+i+" : ");
+            nilaiAkhir[i] = sc.nextInt();
+        }
 
     }
 
