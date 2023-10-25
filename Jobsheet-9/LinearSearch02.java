6acb98ef4caa2bab22a81b0eaf69249b3f2263d5
Khip01 <aakhifathallah24@gmail.com>
2023-10-25 21:21:51 +0700

    feat(pertanyaan): no 2

--- Jobsheet-9/LinearSearch02.java
+++ Jobsheet-9/LinearSearch02.java
@@ -1,13 +1,29 @@
+import java.util.Scanner;
+
 public class LinearSearch02 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         
-        int[] arrayInt = {34, 18, 26, 48, 72, 20, 56, 63};
         int key = 20;
         int hasil = 0;
+        int jumElemenArray = 0;
+
+        System.out.print("Masukkan jumlah elemen array: ");
+        jumElemenArray = sc.nextInt();
+
+        int[] arrayInt = new int[jumElemenArray];
 
         for (int i = 0; i < arrayInt.length; i++) {
-            if (arrayInt[i] == key) {
-                hasil = i;
+            System.out.print("Masukkan elemen array ke-"+i+" : ");
+            arrayInt[i] = sc.nextInt();
+        }
+
+        System.out.print("Masukkan key yang ingin dicari: ");
+        key = sc.nextInt();
+
+        for (int j = 0; j < arrayInt.length; j++) {
+            if (arrayInt[j] == key) {
+                hasil = j;
                 break;
             }
         }
