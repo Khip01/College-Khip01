b4915db64b2a6db349171f8f9c269d0e3c5d8815
Khip01 <aakhifathallah24@gmail.com>
2023-10-25 19:59:59 +0700

    feat(pertanyaan): perubahan statement looping

--- Jobsheet-9/ArrayNilai02.java
+++ Jobsheet-9/ArrayNilai02.java
@@ -8,13 +8,17 @@ public class ArrayNilai02 {
 
         int[] nilaiAkhir = new int[10];
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < nilaiAkhir.length; i++) {
             System.out.print("Masukkan nilai akhir ke-"+i+" : ");
             nilaiAkhir[i] = sc.nextInt();
         }
 
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Nilai akhir ke-"+i+" adalah "+nilaiAkhir[i]);
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            if (nilaiAkhir[i] > 70) {
+                System.out.println("Mahasiswa ke-"+i+" lulus!");
+            } else {
+                System.out.println("Mahasiswa ke-"+i+" tidak lulus!");
+            }
         }
 
     }
