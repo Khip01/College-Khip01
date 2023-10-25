ee558955c1773f7a75a69671d1b46fa29583aa65
Khip01 <aakhifathallah24@gmail.com>
2023-10-25 20:12:24 +0700

    feat: looping input

--- Jobsheet-9/ArrayRataNilai02.java
+++ Jobsheet-9/ArrayRataNilai02.java
@@ -4,6 +4,13 @@ public class ArrayRataNilai02 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        int[] nilaiMhs= new int[10];
+        double total = 0;
+        double rata2;
         
+        for (int i = 0; i < nilaiMhs.length; i++) {
+            System.out.print("masukkan nilai mahasiswa ke-"+(i+1)+" : ");
+            nilaiMhs[i] = sc.nextInt();
+        }
     }
 }
