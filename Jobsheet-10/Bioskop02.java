ebb9633518746c86c4bfe38ddc0f6873c9db8301
Khip01 <aakhifathallah24@gmail.com>
2023-11-01 14:05:58 +0700

    feat: add penonton value, print array length

--- Jobsheet-10/Bioskop02.java
+++ Jobsheet-10/Bioskop02.java
@@ -13,11 +13,18 @@ public class Bioskop02 {
         penonton[2][0] = "Eka";
         penonton[2][1] = "Farhan";
         penonton[3][0] = "Gisel";
+        penonton[3][1] = "Hana";
 
         System.out.printf("%s \t %s\n", penonton[0][0], penonton[0][1]);
         System.out.printf("%s \t %s\n", penonton[1][0], penonton[1][1]);
         System.out.printf("%s \t %s\n", penonton[2][0], penonton[2][1]);
         System.out.printf("%s \t %s\n", penonton[3][0], penonton[3][1]);
+        
+        System.out.println(penonton.length);
+        System.out.println(penonton[0].length);
+        System.out.println(penonton[1].length);
+        System.out.println(penonton[2].length);
+        System.out.println(penonton[3].length);
     }
 
 }
