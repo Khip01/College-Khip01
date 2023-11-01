6f974c20c6767c4e6207d77ebbdebab01792492f
Khip01 <aakhifathallah24@gmail.com>
2023-11-01 14:20:02 +0700

    refactor: print array length

--- Jobsheet-10/Bioskop02.java
+++ Jobsheet-10/Bioskop02.java
@@ -21,10 +21,10 @@ public class Bioskop02 {
         System.out.printf("%s \t %s\n", penonton[3][0], penonton[3][1]);
         
         System.out.println(penonton.length);
-        System.out.println(penonton[0].length);
-        System.out.println(penonton[1].length);
-        System.out.println(penonton[2].length);
-        System.out.println(penonton[3].length);
+
+        for (int i = 0; i < penonton.length; i++) {
+            System.out.println("Panjang baris ke-" + (i+1) + ": " + penonton[i].length);
+        }
     }
 
 }
