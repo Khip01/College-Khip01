704530909e5b11ca901f69182b69f557530b8aef
Khip01 <aakhifathallah24@gmail.com>
2023-11-01 13:30:24 +0700

    feat: add array value

--- Jobsheet-10/Bioskop02.java
+++ Jobsheet-10/Bioskop02.java
@@ -5,7 +5,14 @@ public class Bioskop02 {
 
     public static void main(String[] args) {
         String[][] penonton = new String[4][2];
-         
+
+        penonton[0][0] = "Amin";
+        penonton[0][1] = "Bena";
+        penonton[1][0] = "Candra";
+        penonton[1][1] = "Dela";
+        penonton[2][0] = "Eka";
+        penonton[2][1] = "Farhan";
+        penonton[3][0] = "Gisel";
     }
 
 }
