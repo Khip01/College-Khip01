d2e4e39e2202dfff5dd2b346eb0d2b9ce6973f21
Khip01 <aakhifathallah24@gmail.com>
2023-11-01 13:39:35 +0700

    chore: print array value

--- Jobsheet-10/Bioskop02.java
+++ Jobsheet-10/Bioskop02.java
@@ -13,6 +13,11 @@ public class Bioskop02 {
         penonton[2][0] = "Eka";
         penonton[2][1] = "Farhan";
         penonton[3][0] = "Gisel";
+
+        System.out.printf("%s \t %s\n", penonton[0][0], penonton[0][1]);
+        System.out.printf("%s \t %s\n", penonton[1][0], penonton[1][1]);
+        System.out.printf("%s \t %s\n", penonton[2][0], penonton[2][1]);
+        System.out.printf("%s \t %s\n", penonton[3][0], penonton[3][1]);
     }
 
 }
