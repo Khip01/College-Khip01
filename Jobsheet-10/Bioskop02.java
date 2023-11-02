74d5107b2c37e6ddd6c474abb31652c773be2fcc
Khip01 <aakhifathallah24@gmail.com>
2023-11-03 06:56:44 +0700

    refactor: foreach loop

--- Jobsheet-10/Bioskop02.java
+++ Jobsheet-10/Bioskop02.java
@@ -22,9 +22,10 @@ public class Bioskop02 {
         
         System.out.println(penonton.length);
 
-        for (int i = 0; i < penonton.length; i++) {
-            System.out.println("Panjang baris ke-" + (i+1) + ": " + penonton[i].length);
+        for (String[] barisPenonton : penonton) {
+            System.out.println("Panjang baris: "+ barisPenonton.length);
         }
+        
     }
 
 }
