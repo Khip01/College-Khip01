9cb11e5628e6c139611a86f60eb7c69bcfa7cef2
Khip01 <aakhifathallah24@gmail.com>
2023-11-03 07:36:26 +0700

    feat: add loop process

--- Jobsheet-10/BioskopWithScanner02.java
+++ Jobsheet-10/BioskopWithScanner02.java
@@ -9,6 +9,26 @@ public class BioskopWithScanner02 {
         String nama, next;
 
         String[][] penonton = new String[4][2];
+
+        while (true) {
+            System.out.print("Masukkan nama: ");
+            nama = input.nextLine();
+            System.out.print("Masukkan baris: ");
+            baris = input.nextInt();
+            System.out.print("Masukkan kolom: ");
+            kolom = input.nextInt();
+            input.nextLine();
+
+            penonton[baris-1][kolom-1] = nama;
+
+            System.out.print("Input penonton lainnya? (y/n): ");
+            next = input.nextLine();
+
+            if (next.equalsIgnoreCase("n")) {
+                break;
+            }
+
+        }
     }
 
 }
