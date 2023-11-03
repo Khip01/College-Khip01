bc2600c424652cf70465d4ddf8a450b7cb7996f3
Khip01 <aakhifathallah24@gmail.com>
2023-11-03 09:45:27 +0700

    feat: add loop output

--- Jobsheet-10/Numbers02.java
+++ Jobsheet-10/Numbers02.java
@@ -6,5 +6,9 @@ public class Numbers02 {
         myNumbers[0] = new int[5];
         myNumbers[1] = new int[3];
         myNumbers[2] = new int[1];
+
+        for (int i = 0; i < myNumbers.length; i++) {
+            System.out.println(Arrays.toString(myNumbers[i]));
+        }
     }
 }
