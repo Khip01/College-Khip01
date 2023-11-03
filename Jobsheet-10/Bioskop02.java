80a6e621e0cfc420680894ef66f7939105e26e1c
Khip01 <aakhifathallah24@gmail.com>
2023-11-03 07:26:14 +0700

    feat: init deploy

--- Jobsheet-10/Bioskop02.java
+++ Jobsheet-10/Bioskop02.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Bioskop02
  */
@@ -26,11 +28,12 @@ public class Bioskop02 {
             System.out.println("Panjang baris: "+ barisPenonton.length);
         }
 
-        System.out.println("Penonton pada baris ke-3: ");
+        // System.out.println("Penonton pada baris ke-3: ");
 
-        for (String i : penonton[2]) {
-            System.out.println(i);
+        for (int i = 0; i < penonton.length; i++) {
+            System.out.println("Penonton pada baris ke-"+(i+1)+": "+String.join(", ", penonton[i]));
         }
+        
         
     }
 
