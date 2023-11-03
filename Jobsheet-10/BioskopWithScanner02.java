1dacfe1544cf40977292409cb4a7c22a5d250b90
Khip01 <aakhifathallah24@gmail.com>
2023-11-03 07:29:02 +0700

    feat: declare var

--- Jobsheet-10/BioskopWithScanner02.java
+++ Jobsheet-10/BioskopWithScanner02.java
@@ -3,9 +3,10 @@ import java.util.Scanner;
 public class BioskopWithScanner02 {
     
     public static void main(String[] args) {
-      
+        Scanner input = new Scanner(System.in);
 
-
+        int baris, kolom;
+        String nama, next;
     }
 
 }
