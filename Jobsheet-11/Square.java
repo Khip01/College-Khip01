bf5d0783b79ae415bbd9035764385414b64541c0
Khip01 <aakhifathallah24@gmail.com>
2023-11-07 16:27:27 +0700

    feat: add loop

--- Jobsheet-11/Square.java
+++ Jobsheet-11/Square.java
@@ -3,7 +3,14 @@ import java.util.Scanner;
 public class Square {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); 
+        System.out.print("Masukkan nilai N = "); 
         
+        int N = sc.nextInt(); 
+        
+        for(int i=1; i<=N; i++){ 
+            System.out.print("*"); 
+        }
     }
 
 }
