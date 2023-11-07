63ba34b6fe0a06d68b6c68d78fe7cea22432aed4
Khip01 <aakhifathallah24@gmail.com>
2023-11-07 16:46:44 +0700

    feat: final question modification

--- Jobsheet-11/Square.java
+++ Jobsheet-11/Square.java
@@ -9,7 +9,7 @@ public class Square {
         int N = sc.nextInt(); 
 
         for(int iOuter=1; iOuter<=N; iOuter++){
-            for(int i=1; i<=N; i++){ 
+            for(int i=0; i<=N; i++){ 
                 System.out.print("*"); 
             }
             System.out.println();
