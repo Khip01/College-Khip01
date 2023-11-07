9c2b25d733c4d36fd111aa5d1f0e754c1f8d685a
Khip01 <aakhifathallah24@gmail.com>
2023-11-07 16:29:50 +0700

    feat: add outer loop

--- Jobsheet-11/Square.java
+++ Jobsheet-11/Square.java
@@ -7,9 +7,12 @@ public class Square {
         System.out.print("Masukkan nilai N = "); 
         
         int N = sc.nextInt(); 
-        
-        for(int i=1; i<=N; i++){ 
-            System.out.print("*"); 
+
+        for(int iOuter=1; iOuter<=N; iOuter++){
+            for(int i=1; i<=N; i++){ 
+                System.out.print("*"); 
+            }
+            System.out.println();
         }
     }
 
