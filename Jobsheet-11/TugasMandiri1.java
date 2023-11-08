1908b1dd44c7adba01778c5e78b42e0c3b16bf4e
Khip01 <aakhifathallah24@gmail.com>
2023-11-08 11:51:13 +0700

    refactor: filename

--- Jobsheet-11/TugasMandiri1.java
+++ Jobsheet-11/TugasMandiri1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class TugasMandiri {
+public class TugasMandiri1 {
     public static void main(String[] args) {
         
         Scanner input = new Scanner(System.in);
