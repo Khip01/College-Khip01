59b75d2adf3616a270c748b4c797f78389ba83a9
Khip01 <aakhifathallah24@gmail.com>
2023-11-08 09:29:07 +0700

    feat: final pecobaan

--- Jobsheet-11/Quiz.java
+++ Jobsheet-11/Quiz.java
@@ -3,6 +3,26 @@ import java.util.Random;
 
 public class Quiz {
         public static void main(String[] args) {
-            
+            Random random = new Random();
+            Scanner input = new Scanner(System.in);
+
+            char menu='y'; 
+            do{ 
+                int number = random.nextInt(10) + 1; 
+                boolean success = false; 
+                do { 
+                    System.out.print("Tebak angka (1-10): "); 
+                    int answer = input.nextInt(); 
+                    input.nextLine(); 
+                    if (answer > number) {
+                        System.out.println("# Input anda LEBIH BESAR dari jawaban/number");
+                    } else {
+                        System.out.println("# Input anda LEBIH KECIL dari jawaban/number");
+                    }
+                    success = (answer == number); 
+                } while(!success); 
+                System.out.print("Apakah Anda ingin mengulang permainan (Y/n)?"); 
+                menu = input.nextLine().charAt(0); 
+            } while(menu=='y' || menu=='Y');
         }
 }
