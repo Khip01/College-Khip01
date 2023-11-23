bea38abfc325c4554366fc5308cafdae66044191
Khip01 <aakhifathallah24@gmail.com>
2023-11-23 11:32:28 +0700

    chore: fix typo

--- Jobsheet-13/Ucapan_02.java
+++ Jobsheet-13/Ucapan_02.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
 
 public abstract class Ucapan_02 {
+
+
     public static String PenerimaUcapan(){
-        Scanner sc = new Scanner(Sytem.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Tuliskan NAMA orang yang ingin Anda beri ucapan:");
         String namaOrang = sc.nextLine();
         sc.close();
