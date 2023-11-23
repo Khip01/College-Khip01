6822116fff235d4a8d834c15a9b88dde398d1f64
Khip01 <aakhifathallah24@gmail.com>
2023-11-23 11:34:58 +0700

    feat: final percobaan 2

--- Jobsheet-13/Ucapan_02.java
+++ Jobsheet-13/Ucapan_02.java
@@ -2,6 +2,10 @@ import java.util.Scanner;
 
 public abstract class Ucapan_02 {
 
+    public static void main(String[] args) {
+        String nama = PenerimaUcapan();
+        System.out.println("Thank you "+nama+"\nMay the force be witrh yout.");
+    }
 
     public static String PenerimaUcapan(){
         Scanner sc = new Scanner(System.in);
