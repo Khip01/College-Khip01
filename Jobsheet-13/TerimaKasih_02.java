93157b8306e6ab0736a28e8442107c42a6d65771
Khip01 <aakhifathallah24@gmail.com>
2023-11-23 10:38:56 +0700

    refactor: output without funct

--- Jobsheet-13/TerimaKasih_02.java
+++ Jobsheet-13/TerimaKasih_02.java
@@ -4,10 +4,6 @@
 public class TerimaKasih_02 {
 
     public static void main(String[] args) {
-        UcapanTerimaKasih();
-    }
-
-    public static void UcapanTerimaKasih(){
         System.out.println("Thank you for being the best teacher in the world.\n" +
         "You inspired in me a love for learning and made me feel like I could ask you anything.");
     }
