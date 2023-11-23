c14bfe8e48c8e6725e7e16e4b72a652bd2ac91bc
Khip01 <aakhifathallah24@gmail.com>
2023-11-23 11:02:40 +0700

    feat: final percobaan 2

--- Jobsheet-13/TerimaKasih_02.java
+++ Jobsheet-13/TerimaKasih_02.java
@@ -4,7 +4,17 @@
 public class TerimaKasih_02 {
 
     public static void main(String[] args) {
+        UcapanTerimaKasih();
+        String ucapan = "Terima Kasih Pak.. Bu.. Semoga Sehat Selalu";
+        UcapanTambahan(ucapan);
+    }
+
+    public static void UcapanTerimaKasih(){
         System.out.println("Thank you for being the best teacher in the world.\n" +
         "You inspired in me a love for learning and made me feel like I could ask you anything.");
+    }
+
+    public static void UcapanTambahan(String ucapan){
+        System.out.println(ucapan);
     }
 }
