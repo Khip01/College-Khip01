11781e689ac7b4fbf84675c9ac0234f02e26566a
Khip01 <aakhifathallah24@gmail.com>
2023-11-23 11:31:04 +0700

    feat: add new func

--- Jobsheet-13/Ucapan_02.java
+++ Jobsheet-13/Ucapan_02.java
@@ -1,3 +1,11 @@
+import java.util.Scanner;
+
 public abstract class Ucapan_02 {
-    
+    public static String PenerimaUcapan(){
+        Scanner sc = new Scanner(Sytem.in);
+        System.out.println("Tuliskan NAMA orang yang ingin Anda beri ucapan:");
+        String namaOrang = sc.nextLine();
+        sc.close();
+        return namaOrang;
+    }    
 }
