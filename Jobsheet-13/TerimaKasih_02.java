26ac2ee66dafa3d57769797d5ce849e651b5c904
Khip01 <aakhifathallah24@gmail.com>
2023-11-23 10:27:24 +0700

    feat: add output funct

--- Jobsheet-13/TerimaKasih_02.java
+++ Jobsheet-13/TerimaKasih_02.java
@@ -4,6 +4,10 @@
 public class TerimaKasih_02 {
 
     public static void main(String[] args) {
+        
+    }
+
+    public static void UcapanTerimaKasih(){
         System.out.println("Thank you for being the best teacher in the world.\n" +
         "You inspired in me a love for learning and made me feel like I could ask you anything.");
     }
