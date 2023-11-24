12821ce2e681fe419a41e65c02f64af8d5ac6763
Khip01 <aakhifathallah24@gmail.com>
2023-11-24 07:49:21 +0700

    feat: add program

--- Jobsheet-13/UcapanTerimaKasih_02.java
+++ Jobsheet-13/UcapanTerimaKasih_02.java
@@ -1,8 +1,22 @@
+import java.util.Scanner;
+
 public class UcapanTerimaKasih_02 {
     
     public static void main(String[] args) {
-        
+        UcapanTerimaKasih();
     }
 
-    
+    public static String PenerimaUcapan(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Tuliskan NAMA orang yang ingin Anda beri ucapan:");
+        String namaOrang = sc.nextLine();
+        sc.close();
+        return namaOrang;
+    }
+
+    public static void UcapanTerimaKasih(){
+        String nama = PenerimaUcapan();
+        System.out.println("Thank you "+nama+" for being the best teacher in the world.\n"+
+        "You inspired in me a love for learning and made me feel like I could ask you anything.");
+    }
 }
