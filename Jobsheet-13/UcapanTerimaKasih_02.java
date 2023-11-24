18a3f29d9c53d51bb40ce8c18b2148ac911e0f92
Khip01 <aakhifathallah24@gmail.com>
2023-11-24 08:54:31 +0700

    feat: final pertanyaan

--- Jobsheet-13/UcapanTerimaKasih_02.java
+++ Jobsheet-13/UcapanTerimaKasih_02.java
@@ -4,13 +4,13 @@ public class UcapanTerimaKasih_02 {
     
     public static void main(String[] args) {
         UcapanTerimaKasih();
+        UcapanTambahan();
     }
 
     public static String PenerimaUcapan(){
         Scanner sc = new Scanner(System.in);
         System.out.println("Tuliskan NAMA orang yang ingin Anda beri ucapan:");
         String namaOrang = sc.nextLine();
-        sc.close();
         return namaOrang;
     }
 
@@ -18,5 +18,10 @@ public class UcapanTerimaKasih_02 {
         String nama = PenerimaUcapan();
         System.out.println("Thank you "+nama+" for being the best teacher in the world.\n"+
         "You inspired in me a love for learning and made me feel like I could ask you anything.");
+    }
+
+    public static void UcapanTambahan(){
+        String nama = PenerimaUcapan();
+        System.out.println("Terima kasih "+nama+" sudah menciptakan jobsheet-jobsheet ini, saya sangat senang sekali :)");
     }
 }
