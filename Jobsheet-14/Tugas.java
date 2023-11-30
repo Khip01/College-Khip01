eabfe52959a4f707a70e8a59600533a8cda92a7c
Khip01 <aakhifathallah24@gmail.com>
2023-11-30 10:55:20 +0700

    feat: Tugas No 2 DONE

--- Jobsheet-14/Tugas.java
+++ Jobsheet-14/Tugas.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class Tugas {
+    //// TUGAS NO 1
     // Menampilkan n sampai 0
     static void deretDescendingRekursif(int n){
         if (n == 0) {
@@ -17,16 +18,25 @@ public class Tugas {
         }
     }
 
+    //// TUGAS NO 2
+    // Menghitung penjumlahan bilangan. Misalnya f = 8, 
+    // maka akan dihasilkan 1+2+3+4+5+6+7+8 = 36
+    static int penjumlahanRekursif(int f){
+        if (f == 1) {
+            return (1);
+        } else {
+            return (f + penjumlahanRekursif(f-1));
+        }
+    }
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int n;
+        int f;
 
-        System.out.print("masukkan n: ");
-        n = input.nextInt();
+        System.out.print("masukkan f: ");
+        f = input.nextInt();
 
         System.out.println("Fungsi Rekursif");
-        deretDescendingRekursif(n);
-        System.out.println("Fungsi Iteratif");
-        deretDescendingIteratif(n);
+        System.out.println("Hasil = "+penjumlahanRekursif(f));
     }
 }
