59806161e2685cd7644e61e493ae2a613aa3c15c
Khip01 <aakhifathallah24@gmail.com>
2023-11-30 09:51:29 +0700

    feat: final Pertanyaan 2

--- Jobsheet-14/Percobaan2.java
+++ Jobsheet-14/Percobaan2.java
@@ -3,21 +3,24 @@ import java.util.Scanner;
 public class Percobaan2 {
     static int hitungPangkat(int x, int y) {
         if (y == 0) {
+            System.out.print(1);
             return (1);
         } else {
+            System.out.print(x+"x");
             return (x * hitungPangkat(x, y - 1));
         }
     } 
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int bilangan, pangkat;
+        int bilangan, pangkat, hasil;
         
         System.out.print("Bilangan yang dihitung: ");
         bilangan = sc.nextInt();
         System.out.print("Pangkat: ");
         pangkat = sc.nextInt();
-
-        System.out.println(hitungPangkat(bilangan, pangkat));
+        
+        hasil = hitungPangkat(bilangan, pangkat);
+        System.out.print(" = "+hasil);
     }
 }
