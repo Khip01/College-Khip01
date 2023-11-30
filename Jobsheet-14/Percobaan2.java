8fb426b6627e62ef53c33e3ef8a2747ab5279dff
Khip01 <aakhifathallah24@gmail.com>
2023-11-30 09:31:12 +0700

    feat: declare Scanner lib and new Var

--- Jobsheet-14/Percobaan2.java
+++ Jobsheet-14/Percobaan2.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Percobaan2 {
     static int hitungPangkat(int x, int y) {
         if (y == 0) {
@@ -6,4 +8,11 @@ public class Percobaan2 {
             return (x * hitungPangkat(x, y - 1));
         }
     } 
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int bilangan, pangkat;
+        
+        
+    }
 }
