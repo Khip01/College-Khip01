3ff8d9b8a157be5a1c7b3d3319ee41564578c050
Khip01 <aakhifathallah24@gmail.com>
2023-11-30 09:29:25 +0700

    feat: add hitungPangkat

--- Jobsheet-14/Percobaan2.java
+++ Jobsheet-14/Percobaan2.java
@@ -1,3 +1,9 @@
 public class Percobaan2 {
-    
+    static int hitungPangkat(int x, int y) {
+        if (y == 0) {
+            return (1);
+        } else {
+            return (x * hitungPangkat(x, y - 1));
+        }
+    } 
 }
