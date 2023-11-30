b9f4b58f8ce3badf214bd6969a1382472aa8c329
Khip01 <aakhifathallah24@gmail.com>
2023-11-30 09:07:53 +0700

    chore: fix typo

--- Jobsheet-14/Percobaan1.java
+++ Jobsheet-14/Percobaan1.java
@@ -14,8 +14,10 @@ public class Percobaan1 {
     static int faktorialIteratif(int n) {
         int faktor = 1;
         for (int i = n; i >= 1; i--) {
-            faktor = faktor * 1;
+            faktor = faktor * i;
         }
         return faktor;
     }
+
+
 }
