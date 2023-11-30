7d68e4e03fbf942de0bc6d8f78224c4be3f250a8
Khip01 <aakhifathallah24@gmail.com>
2023-11-30 11:46:34 +0700

    feat: Tugas No 4 DONE

--- Jobsheet-14/Tugas.java
+++ Jobsheet-14/Tugas.java
@@ -1,58 +1,28 @@
 import java.util.Scanner;
 
 public class Tugas {
-    //// TUGAS NO 1
-    // Menampilkan n sampai 0
-    static void deretDescendingRekursif(int n){
-        if (n == 0) {
-            System.out.println(n);
+    //// TUGAS NO 4
+    // Berapa pasangan marmut yang ada pada akhir bulan ke-12? 
+    // Buatlah programnya menggunakan fungsi rekursif! (Fibonacci)
+    // 1 + (1 + (2 + (3) + (5)))
+    static int fibonacciMarmut(int bulan, int pasangan, int pasanganBefore){
+        if (bulan == 1) {
+            return pasangan;
         } else {
-            System.out.print(n+", ");
-            deretDescendingRekursif(n-1);
-        }
-    }
-
-    static void deretDescendingIteratif(int n){
-        for (int i = n; i >= 0; i--) {
-            System.out.print(i == 0 ? i : i+", ");
-        }
-    }
-
-    //// TUGAS NO 2
-    // Menghitung penjumlahan bilangan. Misalnya f = 8, 
-    // maka akan dihasilkan 1+2+3+4+5+6+7+8 = 36
-    static int penjumlahanRekursif(int f){
-        if (f == 1) {
-            return (1);
-        } else {
-            return (f + penjumlahanRekursif(f-1));
-        }
-    }
-
-    //// TUGAS NO 3
-    // mengecek apakah suatu bilangan n merupakan 
-    // bilangan prima atau bukan.
-    static boolean cekPrimaRekursif(int n, int m){
-        if (n == m) {
-            m = n-1;
-        }
-        if (m == 1) {
-            return true;
-        } else if (n % m == 0) {
-            return false;
-        } else {
-            return cekPrimaRekursif(n, m-1);
+            return fibonacciMarmut(bulan-1, pasangan + pasanganBefore, pasangan);
         }
     }
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int n;
+        int bulan, hasil;
 
-        System.out.print("masukkan n: ");
-        n = input.nextInt();
+        System.out.print("masukkan bulan: ");
+        bulan = input.nextInt();
 
         System.out.println("Fungsi Rekursif");
-        System.out.println("Apakah dia prima? "+cekPrimaRekursif(n, n));
+        // Nilai default param pasangan = 1 dan pasangan before = 0 untuk deklarasi bulan ke 1
+        hasil = fibonacciMarmut(bulan, 1, 0);
+        System.out.println("Total Pasangan di bulan ke-"+bulan+" adalah "+hasil);
     }
 }
