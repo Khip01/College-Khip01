83ec968f1e8ce0a129767b8f45ef1009521c653e
Khip01 <aakhifathallah24@gmail.com>
2023-11-30 09:08:45 +0700

    feat: add output

--- Jobsheet-14/Percobaan1.java
+++ Jobsheet-14/Percobaan1.java
@@ -19,5 +19,9 @@ public class Percobaan1 {
         return faktor;
     }
 
+    public static void main(String[] args) {
+        System.out.println(faktorialRekursif(5));
+        System.out.println(faktorialIteratif(5));
+    }
 
 }
