57e22c774f73ad90d421a87d7aaefabb3e278ee2
Khip01 <aakhifathallah24@gmail.com>
2023-11-30 11:48:20 +0700

    feat: final Tugas

--- Jobsheet-14/Tugas.java
+++ Jobsheet-14/Tugas.java
@@ -1,6 +1,50 @@
 import java.util.Scanner;
 
 public class Tugas {
+    //// TUGAS NO 1
+    // Menampilkan n sampai 0
+    static void deretDescendingRekursif(int n){
+        if (n == 0) {
+            System.out.println(n);
+        } else {
+            System.out.print(n+", ");
+            deretDescendingRekursif(n-1);
+        }
+    }
+
+    static void deretDescendingIteratif(int n){
+        for (int i = n; i >= 0; i--) {
+            System.out.print(i == 0 ? i : i+", ");
+        }
+    }
+
+    //// TUGAS NO 2
+    // Menghitung penjumlahan bilangan. Misalnya f = 8, 
+    // maka akan dihasilkan 1+2+3+4+5+6+7+8 = 36
+    static int penjumlahanRekursif(int f){
+        if (f == 1) {
+            return (1);
+        } else {
+            return (f + penjumlahanRekursif(f-1));
+        }
+    }
+
+    //// TUGAS NO 3
+    // mengecek apakah suatu bilangan n merupakan 
+    // bilangan prima atau bukan.
+    static boolean cekPrimaRekursif(int n, int m){
+        if (n == m) {
+            m = n-1;
+        }
+        if (m == 1) {
+            return true;
+        } else if (n % m == 0) {
+            return false;
+        } else {
+            return cekPrimaRekursif(n, m-1);
+        }
+    }
+
     //// TUGAS NO 4
     // Berapa pasangan marmut yang ada pada akhir bulan ke-12? 
     // Buatlah programnya menggunakan fungsi rekursif! (Fibonacci)
