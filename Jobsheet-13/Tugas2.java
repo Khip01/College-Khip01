e6b1579097ba9b0130ad923f4919db89ba26a5a3
Khip01 <aakhifathallah24@gmail.com>
2023-11-30 09:02:39 +0700

    chore: idk

--- Jobsheet-13/Tugas2.java
+++ Jobsheet-13/Tugas2.java
@@ -1,33 +1,41 @@
 import java.util.Scanner;
 
 public class Tugas2 {
-    // static int dataNilai[][] = new int[5][7];
-    static int dataNilai[][] = {{20, 19, 25, 20, 10, 0, 10}, {30, 30, 40, 10, 15, 20, 25}, {5, 0, 20, 25, 10, 5, 45}, {50, 0, 7, 8, 0, 30, 60}, {15, 10, 16, 15, 10, 10, 5}};
+    // static int dataNilai[][] = {{20, 19, 25, 20, 10, 0, 10}, {30, 30, 40, 10, 15, 20, 25}, {5, 0, 20, 25, 10, 5, 45}, {50, 0, 7, 8, 0, 30, 60}, {15, 10, 16, 15, 10, 10, 5}};
     static Scanner input = new Scanner(System.in);
+    static int dataNilai[][];
+    static String namaMhs[];
 
-    static void inputDataMhs(){
+
+    static void inputNamaMhs(int jumMhs){
+        input.nextLine();
+        String[] arrayInput = new String[jumMhs];
+        namaMhs = arrayInput;
+
+        for (int i = 0; i < namaMhs.length; i++) {
+            System.out.print("Masukkan Nama Mahasiswa ke "+(i+1)+": ");
+            namaMhs[i] = input.nextLine();
+        }
+    }
+
+    static void inputDataMhs(int[][] arrayInput){
+        dataNilai = arrayInput;
         for (int i = 0; i < dataNilai.length; i++) {
             for (int j = 0; j < dataNilai[i].length; j++) {
-                System.out.print("Baris ke "+(i+1)+" minggu ke-"+(j+1)+" : ");
+                System.out.print("Nilai "+namaMhs[i]+" minggu ke-"+(j+1)+" : ");
                 dataNilai[i][j] = input.nextInt();
             }
         }
     }
 
-    static void tampilDataMhs(){
-        System.out.println("\t\tMinggu ke-1\tMinggu ke-2\tMinggu ke-3\tMinggu ke-4\tMinggu ke-5\tMinggu ke-6\tMinggu ke-7");
+    static void tampilDataMhs(int jumMinggu){
+        System.out.print("\t");
+        for (int i = 0; i < jumMinggu; i++) {
+            System.out.print("\tMinggu ke-"+(i+1));
+        }
+        System.out.println();
         for (int i = 0; i < dataNilai.length; i++) {
-            if (i == 0) {
-                System.out.print("Sari\t\t");
-            } else if (i == 1) {
-                System.out.print("Rina\t\t");
-            } else if (i == 2) {
-                System.out.print("Yani\t\t");
-            } else if (i == 3) {
-                System.out.print("Dwi\t\t");
-            } else if (i == 4) {
-                System.out.print("Lusi\t\t");
-            }
+            System.out.print(namaMhs[i]+"\t\t");
             for (int j = 0; j < dataNilai[i].length; j++) {
                 System.out.print(dataNilai[i][j] + "\t\t");
             }
@@ -50,40 +58,35 @@ public class Tugas2 {
     }
 
     static void searchNamaNilaiMinggu(){
-        int nilaiTertinggi = dataNilai[0][0], mingguKe = 1, indexMhs = 1;
+        int nilaiTertinggi = dataNilai[0][0], mingguKe = 1, indexMhs = 0;
 
         for (int i = 0; i < dataNilai.length; i++) {
             for (int j = 1; j < dataNilai[i].length; j++) {
                 if (dataNilai[i][j] > nilaiTertinggi) {
                     nilaiTertinggi = dataNilai[i][j];
                     mingguKe = j+1;
-                    indexMhs = i+1;
+                    indexMhs = i;
                 }
             }
         }
 
-        switch (indexMhs) {
-            case 1:
-                System.out.println("Nilai tertinggi diperoleh oleh Sari dengan nilai "+nilaiTertinggi+" Minggu ke "+mingguKe);
-                break;
-            case 2:
-                System.out.println("Nilai tertinggi diperoleh oleh Rina dengan nilai "+nilaiTertinggi+" Minggu ke "+mingguKe);
-                break;
-            case 3:  
-                System.out.println("Nilai tertinggi diperoleh oleh Yani dengan nilai "+nilaiTertinggi+" Minggu ke "+mingguKe);
-                break;
-            case 4:
-                System.out.println("Nilai tertinggi diperoleh oleh Dwi dengan nilai "+nilaiTertinggi+" Minggu ke "+mingguKe);
-                break;
-            case 5:
-                System.out.println("Nilai tertinggi diperoleh oleh Lusi dengan nilai "+nilaiTertinggi+" Minggu ke "+mingguKe);
-                break;
-        }
+        System.out.println("Nilai tertinggi diperoleh oleh "+namaMhs[indexMhs]+" dengan nilai "+nilaiTertinggi+" Minggu ke "+mingguKe);
     }
 
     public static void main(String[] args) {
-        // inputDataMhs();
-        tampilDataMhs();
+        int jumlahMhs, jumlahMinggu;
+
+        System.out.print("Masukkan jumlah Mahasiswa: ");
+        jumlahMhs = input.nextInt();
+        inputNamaMhs(jumlahMhs);
+
+        System.out.print("Masukkan jumlah Minggu/Tugas: ");
+        jumlahMinggu = input.nextInt();
+
+        int dataNilai[][] = new int[jumlahMhs][jumlahMinggu];
+
+        inputDataMhs(dataNilai);
+        tampilDataMhs(jumlahMinggu);
         searchNilaiTertinggi();
         searchNamaNilaiMinggu();
     }
